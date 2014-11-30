2d0b81a1d517f1ab784037e881a4d7580ba340dc
Terek <terek.arce@gmail.com>
2014-11-30 15:19:10 -0500

    adding reg file

--- src/mipSim/pipeline/units/ROB.java
+++ src/mipSim/pipeline/units/ROB.java
@@ -1,5 +1,26 @@
 package mipSim.pipeline.units;
 
-public class ROB {
+import java.util.LinkedList;
 
+public class ROB {
+	
+	private LinkedList <Integer> temporary;
+	private LinkedList <Integer> finalized;
+	
+	public ROB() {
+		temporary = new LinkedList <Integer> ();
+		finalized = new LinkedList <Integer> ();
+	}
+	
+	/**
+	 * Sync the two internal buffers.
+	 */
+	public void sync() {
+		
+		while(!temporary.isEmpty()) {
+			finalized.add(temporary.removeFirst());
+		}
+		temporary.clear();
+	}
+	
 }
--- src/mipSim/pipeline/units/RegFile.java
+++ src/mipSim/pipeline/units/RegFile.java
@@ -1,5 +1,51 @@
 package mipSim.pipeline.units;
 
 public class RegFile {
+	private Integer [] temporary;
+	private Integer [] finalized;
+	
+	/**
+	 * Create Registers initialized to 0.
+	 */
+	public RegFile() {
+		temporary = new Integer [32];
+		finalized = new Integer [32];
+		
+		int i;
+		for (i = 0; i < temporary.length; i++) {
+			temporary[i] = 0;
+		}
+		
+		for (i = 0; i < finalized.length; i++) {
+			finalized[i] = 0;
+		}
+	}
+	
+	/**
+	 * Sync the two internal buffers.
+	 */
+	public void sync() {
+		
+		for (int i = 0; i <= temporary.length - 1; i++) {
+			finalized[i] = temporary[i];
+		}
+
+//TODO:		temporary.clear();
+	}
+	
+	/**
+	 * Prints the contents of the RegisterFile
+	 */
+	public void printContents() {
+		
+//		Collections.sort(finalized);
+		
+		System.out.print("RF:");
+		
+		for (int i = 0; i < finalized.length - 1; i++) {
+			System.out.print(finalized[i]+ ",");
+		}
+		System.out.print(finalized[finalized.length - 1] + "\n");
+	}
 
 }
