c1a506aaa7bcea310dabf0e1ebfd045e0392202f
Terek Arce <terek.arce@gmail.com>
2018-07-18 10:57:18 -0400

    Added initializer, helper methods
    Added initializers for private lists and added parse methods and
    helpers.

--- Assets/DisassemblerControl.cs
+++ Assets/DisassemblerControl.cs
@@ -23,6 +23,12 @@ namespace MIPS
         private static List<string> _binary;
         private static List<string> _instructions;
 
+        static Disassembler()
+        {
+            _binary = new List<string>();
+            _instructions = new List<string>();
+        }
+
         /// <summary>
         /// 
         /// </summary>
@@ -49,16 +55,20 @@ namespace MIPS
         /// <param name="word"></param>
         private static void disassemble(byte[] word)
         {
-            Debug.Log("Dissasembling");
             if (word.Length < 4)
             {
                 Debug.Log("Error: byte[].length < 32 bits");
                 return;
             }
-            BitArray bitArrWord = new BitArray(word);
-            Debug.Log(wordToStr(word));
+            _binary.Add(wordToStr(word));
+            parse(word);
         }
 
+        /// <summary>
+        /// The word2Str method converts a byte array to its string representation.
+        /// </summary>
+        /// <param name="word">A byte[] of lenth 4 (4 bytes = word)</param>
+        /// <returns>Returns a string representing the binary of the 32 bit word.</returns>
         private static string wordToStr(byte[] word)
         {
             if (word.Length < 4)
@@ -71,6 +81,40 @@ namespace MIPS
                 Convert.ToString(word[2], 2).PadLeft(8, '0') +
                 Convert.ToString(word[3], 2).PadLeft(8, '0') ;
             return bitString;
+        }
+
+        private static void parse(byte[] word)
+        {
+            BitArray bitArrWord = new BitArray(word);
+            BitArray opcodeMask = new BitArray(32);
+            for (int bit = 0; bit < 6; bit++)
+            {
+                opcodeMask[bit] = true;
+            }
+
+            Debug.Log(toString(opcodeMask));
+        }
+
+        public static string toString(BitArray bitArray)
+        {
+            string values = "";
+            foreach (bool bit in bitArray)
+            {
+                values += (bit == true) ? "1" : "0";
+            }
+            return values;
+        }
+
+        private static int getIntFromBitArray(BitArray bitArray)
+        {
+
+            if (bitArray.Length > 32)
+                throw new ArgumentException("Argument length shall be at most 32 bits.");
+
+            int[] array = new int[1];
+            bitArray.CopyTo(array, 0);
+            return array[0];
+
         }
     }
 }
