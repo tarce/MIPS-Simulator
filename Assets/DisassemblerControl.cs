c3d21e60b29ef5896fb0c6e2b739a45d5493f69a
Terek Arce <terek.arce@gmail.com>
2018-07-17 22:58:28 -0400

    Updated Disassembler
    Added methods to handle reading binary file.  Altered binary file
    extension so it can be read by unity text asset.

---
+++ Assets/DisassemblerControl.cs
@@ -0,0 +1,78 @@
+﻿
+namespace MIPS
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using System.IO;
+
+    public class DisassemblerControl : MonoBehaviour
+    {
+        public TextAsset binaryFile;
+
+        public void Disassemble()
+        {
+            Disassembler.readBinaryFile(binaryFile.bytes);
+        }
+    }
+
+    static class Disassembler
+    {
+        private static List<string> _binary;
+        private static List<string> _instructions;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        public static void readBinaryFile(byte[] binaryFile)
+        {
+            Stream ms = new MemoryStream(binaryFile);
+            using (BinaryReader br = new BinaryReader(ms))
+            {
+                while (br.BaseStream.Position != br.BaseStream.Length)
+                {
+                    // Read the source file into a byte array, 
+                    // 32 bits at a time (4 bytes = 1 word)
+                    byte[] word = br.ReadBytes(4);
+                    disassemble(word);
+                }
+                br.Close();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="word"></param>
+        private static void disassemble(byte[] word)
+        {
+            if (word.Length < 4)
+            {
+                Debug.Log("Error: byte[].length < 32 bits");
+                return;
+            }
+
+            BitArray bitWord = new BitArray(word);
+            Debug.Log(BitArrayToStr(bitWord));
+        }
+
+        private static string BitArrayToStr(BitArray ba)
+        {
+            byte[] strArr = new byte[ba.Length / 8];
+
+            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+
+            for (int i = 0; i < ba.Length / 8; i++)
+            {
+                for (int index = i * 8, m = 1; index < i * 8 + 8; index++, m *= 2)
+                {
+                    strArr[i] += ba.Get(index) ? (byte)m : (byte)0;
+                }
+            }
+
+            return encoding.GetString(strArr);
+        }
+    }
+}
+
